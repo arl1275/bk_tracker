b1dcb06a43302bc650d422d0323892d5b3c4598c
Jose Mendoza <jmendoza@intermoda.com.hn>
2024-02-07 19:12:36 -0600

    FIRST VERSION

---
+++ src/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const {
+  NODE_ENV = 'production',
+} = process.env;
+
+module.exports = {
+  mode: NODE_ENV,
+  entry: './src/index.ts',
+  target: 'node',
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include : /src/,
+        use: [
+          'ts-loader',
+        ],
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  }
+}
+
